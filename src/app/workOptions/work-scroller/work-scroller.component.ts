0541d811b52c1f0a320d78107aba6fe089ea4131
Charlie Palm <cwpalm@wisc.edu>
2025-04-03 16:49:00 -0500

    main | add scrolling for mobile

--- src/app/workOptions/work-scroller/work-scroller.component.ts
+++ src/app/workOptions/work-scroller/work-scroller.component.ts
@@ -13,6 +13,7 @@ export class WorkScrollerComponent implements OnInit, OnDestroy {
   @Input() downOption?: WorkOption;
   doneInitting = false;
   timerSub!: Subscription;
+  touches = [0, 0];
   constructor(private router: Router) {}
   ngOnInit(): void {
     this.timerSub = timer(500).subscribe((_) => this.doneInitting = true);
@@ -24,13 +25,27 @@ export class WorkScrollerComponent implements OnInit, OnDestroy {
 
   @HostListener('window:wheel', ['$event'])
   @HostListener('window:touchmove', ['$event'])
-  reroute(event: WheelEvent) {
-    if (this.doneInitting) {
+  reroute(event: WheelEvent | TouchEvent) {
+    if (this.doneInitting && event instanceof WheelEvent) {
       if (this.downOption && event.deltaY > 0) {
         this.router.navigate([this.downOption]);
       } else if (this.upOption && event.deltaY < 0) {
         this.router.navigate([this.upOption]);
       }
+    } else if (event instanceof TouchEvent && event.touches.item(0)) {
+      this.touches.pop();
+      this.touches.unshift(event.touches.item(0)!.clientY);
+    }
+  }
+
+  @HostListener('window:touchend')
+  touchend() {
+    if (this.upOption && this.touches[0] - this.touches[1] > 0) {
+      this.router.navigate([this.upOption]);
+    } else if (this.downOption && this.touches[0] - this.touches[1] < 0) {
+      this.router.navigate([this.downOption]);
+    } else {
+      // this is a tap event
     }
   }
 }
