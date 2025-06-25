c0d8c5d0d424ccf416d87f0b3e0c799130d92dc1
Charlie Palm <cwpalm@wisc.edu>
2025-06-24 19:44:13 -0500

    main | add analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { routeAnimations } from './route.animations';
+import { environment } from 'src/environments/environment';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: false,
-  animations: [routeAnimations]
+  animations: [routeAnimations],
 })
 export class AppComponent {
   title = '';
-  constructor(public router: Router) {}
+  constructor(public router: Router) {
+    if (environment.production) {
+      inject();
+    }
+  }
 }
